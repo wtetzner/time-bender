a3bb23028232cd60b85e6fa0bf9e3b114b2011f8
Walter Tetzner <walter@waltertetzner.net>
2011-02-06 01:14:55 -0500

    Initial Commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject org.bovinegenius/date-time "1.0.0-SNAPSHOT"
+  :description "Date-Time library that defines a DateTime protocol and
+  implements it on various Java types."
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
