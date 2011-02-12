e71a42a43a45deca25beec39ed22c1c45fcf1dc4
Walter Tetzner <walter@waltertetzner.net>
2011-02-12 16:38:31 -0500

    Added parser and parse functions, to allow creating a parser function and to parse a string into a DateTime. Also added Datable interface, and implemented it on java.util.Date, java.util.Calendar, and Joda Time DateTime.

--- src/org/bovinegenius/date_time.clj
+++ src/org/bovinegenius/date_time.clj
@@ -1,3 +1,43 @@
-(ns org.bovinegenius.date-time)
+(ns org.bovinegenius.date-time
+  (:import (org.joda.time DateTime)
+           (org.joda.time.format DateTimeFormat)))
+
+(defprotocol Datable
+  "A protocol to convert to and from Joda Time DateTime objects."
+  (as-date-time [self] "Convert to Joda Time DateTime.")
+  (from-date-time [self date-time] "Convert from Joda Time DateTime."))
+
+(defn parser
+  "Returns a function that takes a string and parses it into a
+DateTime using the given format string."
+  [format]
+  (let [formatter (DateTimeFormat/forPattern format)]
+    (fn [string]
+      (.parseDateTime formatter string))))
+
+(defn parse
+  "Parse a string into a DateTime."
+  [string format]
+  ((parser format) string))
+
+(extend java.util.Date
+  Datable
+  {:as-date-time (fn [self]
+                   (-> self .getTime DateTime.))
+   :from-date-time (fn [self date-time]
+                     (-> date-time .getMillis java.util.Date.))})
+
+(extend DateTime
+  Datable
+  {:as-date-time identity
+   :from-date-time (fn [self date-time]
+                     date-time)})
+
+(extend java.util.Calendar
+  Datable
+  {:as-date-time (fn [self]
+                   (as-date-time (.getTime self)))
+   :from-date-time (fn [self date-time]
+                     (-> self .clone (.setTimeInMillis (.getMillis date-time))))})
 
 
